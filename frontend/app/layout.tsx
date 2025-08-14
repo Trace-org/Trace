5d0aa173b738c8894b0041e13f608cba05263591
mariaelisaaraya <maria.elisa.araya.work@gmail.com>
2025-08-14 00:36:51 -0300

    feat: add layout

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -2,6 +2,7 @@ import type React from "react"
 import type { Metadata } from "next"
 import { Inter } from "next/font/google"
 import "./globals.css"
+import { Provider } from "@/hooks/Provider" 
 
 const inter = Inter({
   subsets: ["latin"],
@@ -22,7 +23,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="es" className={`${inter.variable}`}>
-      <body className="font-sans antialiased">{children}</body>
+      <body className="font-sans antialiased">
+        <Provider> 
+          {children}
+        </Provider>
+      </body>
     </html>
   )
 }
